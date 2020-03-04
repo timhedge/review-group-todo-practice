5e6f2552ae379987774937835bf3cc8ae1f5336c
Tim Hedge <timwhedge@gmail.com>
2020-03-04 09:04:59 -0600

    adds some code for app and index

---
+++ client/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            groceryItem: '',
+            groceryList: []
+        }
+    }
+}
+
+// functions
+
+// render
+render() {
+    <div>this is a test
+    </div>
+}
